d5c35c664dfb37554886ff7cb858fdc74b8f2bbd
avogis <sigova@kth.se>
2018-03-12 19:05:48 +0100

    Send email on save

--- accreditation/accreditation_app/models.py
+++ accreditation/accreditation_app/models.py
@@ -1,10 +1,40 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from postmark.core import PMMail, PMMailSendException
+
+from accreditation.settings import (
+    POSTMARK_APPLICATION_SENT_TEMPLATE,
+    POSTMARK_SENDER,
+)
 
 
 class AccreditatonApplication(models.Model):
     first_name = models.CharField(null=False, blank=False, max_length=50, default='')
     last_name = models.CharField(null=False, blank=False, max_length=50, default='')
     email = models.EmailField(null=False, blank=False, default='')
+    reviewed = models.BooleanField(default=False)
 
     def __str__(self):
         return "{} {} {}".format(self.first_name, self.last_name, self.email)
+
+    def save(self, force_insert=False, force_update=False, *args, **kwargs):
+        if self.reviewed:
+            data = {
+                'first_name': self.first_name,
+                'last_name': self.last_name,
+                'email': self.email,
+            }
+            try:
+                pm = PMMail(
+                    to=self.email,
+                    sender=POSTMARK_SENDER,
+                    template_id=POSTMARK_APPLICATION_SENT_TEMPLATE,
+                    template_model=data
+                )
+                pm.send()
+                super(AccreditatonApplication, self).save(*args, **kwargs)
+            except PMMailSendException:
+                raise Exception
+        else:
+            super(AccreditatonApplication, self).save(*args, **kwargs)
--- accreditation/accreditation_app/tests/test_models.py
+++ accreditation/accreditation_app/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
+from mock import Mock, patch
 
 from accreditation_app.models import AccreditatonApplication
 
@@ -53,3 +54,32 @@ class AccreditatonApplicationModelTest(TestCase):
         with self.assertRaises(ValidationError):
             application.save()
             application.full_clean()
+
+    @patch('accreditation_app.models.PMMail', return_value=Mock())
+    def test_when_reviewed_email_is_sent(self, mock_pm):
+        mock_pm.send().return_value = 'OK'
+
+        first_name = 'Emily',
+        last_name = 'Vargas',
+        email = 'emily.vargas@test.com'
+
+        application = AccreditatonApplication.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            email=email
+        )
+        application.save()
+        application = AccreditatonApplication.objects.get(pk=1)
+        application.reviewed = True
+        data = {
+            'first_name': "{}".format(first_name),
+            'last_name': "{}".format(last_name),
+            'email': "{}".format(email),
+        }
+        application.save()
+        mock_pm.assert_called_with(
+            sender='elizaveta@popaganda.se',
+            template_id=5293163,
+            template_model=data,
+            to='emily.vargas@test.com'
+        )
